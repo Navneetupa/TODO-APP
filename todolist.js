5dfdf111ecbd7efcb34002baf0db8098d8e9cf01
navneetupa <navneetupa.1000@gmail.com>
2025-01-28 23:58:59 +0530

    first commit

---
+++ todolist.js
@@ -0,0 +1,42 @@
+function addTask() {
+  
+  const taskText = document.createElement('textarea');
+  taskText.textContent = document.getElementById('inputTask').value;
+
+  if (taskText.textContent.trim() === "") {
+    alert("Please enter the text first");
+    return; 
+  }
+  const newTask = document.createElement('li');
+  const taskList = document.getElementById('taskList');
+  taskList.appendChild(newTask);
+
+  newTask.appendChild(taskText);
+
+  document.getElementById('inputTask').value = "";
+
+  deleteTask(newTask);
+  updateTask(newTask, taskText); 
+}
+
+function deleteTask(newTask) {
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = "Delete";
+  newTask.appendChild(deleteBtn);
+
+  deleteBtn.onclick = function () {
+    newTask.remove();
+  };
+}
+
+function updateTask(newTask, taskText) {
+  const updateBtn = document.createElement('button');
+  updateBtn.id = "todoSpan"
+  updateBtn.textContent = "Update";
+  newTask.appendChild(updateBtn);
+
+  updateBtn.onclick = function () {
+    const newInput = prompt("Update your task:", taskText.textContent);
+    if (newInput) taskText.textContent = newInput; 
+  };
+}
